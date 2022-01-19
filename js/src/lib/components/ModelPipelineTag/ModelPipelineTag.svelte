a833c550205e9a0f3bf413129c1a20322c6cea31
Thibault Goehringer <tgoehringer@gmail.com>
2022-01-19 11:08:59 +0100

    Update widget links (#605)

--- js/src/lib/components/ModelPipelineTag/ModelPipelineTag.svelte
+++ js/src/lib/components/ModelPipelineTag/ModelPipelineTag.svelte
@@ -1,18 +1,18 @@
-<<<<<<< HEAD:widgets/src/lib/ModelPipelineTag/ModelPipelineTag.svelte
 <script>
 	import { PipelineType } from "$lib/interfaces/Types";
-=======
-<script lang="ts">
-	import { PipelineType } from "../../interfaces/Types";
->>>>>>> 4ecdf28d (Shared components directory (#579)):js/src/lib/components/ModelPipelineTag/ModelPipelineTag.svelte
 	import ModelPipelineIcon from "../ModelPipelineIcon/ModelPipelineIcon.svelte";
 
 	export let pipeline = "";
 </script>
 
-<div class="inline-flex items-center">
+<a
+	class="inline-flex items-center hover:underline {classNames}"
+	href="/tasks/{pipeline}"
+	target="_blank"
+	title="Learn more about {PipelineType[pipeline]}"
+>
 	<ModelPipelineIcon classNames="mr-1" {pipeline} />
 	<span>
 		{PipelineType[pipeline] ?? pipeline}
 	</span>
-</div>
+</a>
