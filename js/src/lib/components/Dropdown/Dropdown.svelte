90873d4621415d3ced486173bbfc17a418239e98
Thibault Goehringer <tgoehringer@gmail.com>
2022-03-21 15:13:00 +0100

    Add "disabled" prop to Dropdown
    https://github.com/huggingface/huggingface_hub/pull/774

--- js/src/lib/components/Dropdown/Dropdown.svelte
+++ js/src/lib/components/Dropdown/Dropdown.svelte
@@ -9,32 +9,15 @@
 	export let btnIcon: typeof SvelteComponent | undefined = undefined;
 	export let btnIconClassNames = "";
 	export let btnLabel = "";
+	export let disabled = false;
 	export let entries: Array<{
 		classNames?: string;
 		href?: string;
-		icon?: typeof SvelteComponent;
-		iconClassNames?: string;
-		onClick?: (e: MouseEvent) => void;
-		label?: string;
-		noFollow?: boolean;
-		underline?: boolean;
-		targetBlank?: boolean;
-	}> = [];
-	export let forceMenuAlignement: "left" | "right" | undefined = undefined;
-	export let menuClassNames = "";
-	export let noBtnClass: boolean | undefined = undefined;
-	export let withBtnCaret = false;
-
-	let element: HTMLElement | undefined = undefined;
-	let isOpen = false;
-</script>
-
-<div class="relative {classNames}" bind:this={element}>
-	<!-- Button -->
+	@@ -34,7 +35,7 @@
 	<button
 		class="{btnClassNames}
 			{!noBtnClass ? 'cursor-pointer w-full btn text-sm' : ''}"
-		on:click={() => (isOpen = !isOpen)}
+		on:click={() => (isOpen = !disabled && !isOpen)}
 		type="button"
 	>
 		<!-- The "button" slot can overwrite the defaut button content -->
