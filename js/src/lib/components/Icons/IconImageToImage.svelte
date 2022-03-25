9dc930bfaa6904f0725004ad4f52d0ccb429a4d0
nateraw <nxr9266@g.rit.edu>
2022-03-25 15:33:25 -0400

    :lipstick: prettier

--- js/src/lib/components/Icons/IconImageToImage.svelte
+++ js/src/lib/components/Icons/IconImageToImage.svelte
@@ -15,7 +15,7 @@
 	preserveAspectRatio="xMidYMid meet"
 	viewBox="0 0 32 32"
 >
-    <path
+	<path
 		fill="currentColor"
 		d="M4 22H2V4a2.002 2.002 0 0 1 2-2h18v2H4zm17-5a3 3 0 1 0-3-3a3.003 3.003 0 0 0 3 3zm0-4a1 1 0 1 1-1 1a1 1 0 0 1 1-1z"
 	/><path
