f29e950e870a5a28cc2c92f9260cd7aa9df268be
Julien Chaumond <julien@huggingface.co>
2022-04-13 16:16:15 +0200

    when exporting tasks, honor `hideInDatasets`

--- js/src/scripts/export-tasks.ts
+++ js/src/scripts/export-tasks.ts
@@ -1,13 +1,19 @@
 import { PIPELINE_DATA } from "../lib/interfaces/Types";
 
+const DISABLE_HIDE_IN_DATASETS = process.argv.slice(2).includes("---no-hide-in-datasets") ?? true;
+
 (async () => {
 	const tasks = Object.fromEntries(
 		Object.entries(PIPELINE_DATA).map(([k, v]) => {
+			if (!DISABLE_HIDE_IN_DATASETS && v.hideInDatasets) {
+				return;
+			}
 			return [k, {
 				type:     v.modality,
 				subtasks: v.subtasks?.map(sub => sub.type),
 			}];
 		})
+			.filter(x => !!x)
 			.sort((a, b) => a[0].toString().localeCompare(b[0].toString()))
 			/// ^ remove this line when you don't need tasks.json sorted
 	);
