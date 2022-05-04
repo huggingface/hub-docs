21f60bf2e298e132e665e53e547b41a68e77fd05
Mishig Davaadorj <dmishig@gmail.com>
2022-05-04 10:14:35 +0200

    Support tapex for QA widget

--- js/src/lib/components/InferenceWidget/widgets/TableQuestionAnsweringWidget/TableQuestionAnsweringWidget.svelte
+++ js/src/lib/components/InferenceWidget/widgets/TableQuestionAnsweringWidget/TableQuestionAnsweringWidget.svelte
@@ -45,6 +45,7 @@
 	let outputJson: string;
 	let table: (string | number)[][] = [[]];
 	let query = "";
+	let isAnswerOnlyOutput = false;
 
 	let highlighted: HighlightCoordinates = {};
 	$: highlighted =
@@ -139,17 +140,23 @@
 			arg &&
 			typeof arg === "object" &&
 			typeof arg["answer"] === "string" &&
-			Array.isArray(arg["coordinates"]) &&
-			Array.isArray(arg["cells"])
+			(arg["aggregator"] === undefined
+				? true
+				: typeof arg["aggregator"] === "string") &&
+			(Array.isArray(arg["coordinates"]) || isAnswerOnlyOutput) &&
+			(Array.isArray(arg["cells"]) || isAnswerOnlyOutput)
 		);
 	}
 
 	function parseOutput(body: unknown): Output {
+		if (body["coordinates"] === undefined && body["cells"] === undefined) {
+			isAnswerOnlyOutput = true;
+		}
 		if (isValidOutput(body)) {
 			return body;
 		}
 		throw new TypeError(
-			"Invalid output: output must be of type <answer:string; coordinates:Array; cells:Array>"
+			"Invalid output: output must be of type <answer:string; coordinates?:Array; cells?:Array; aggregator?:string>"
 		);
 	}
 
@@ -189,7 +196,7 @@
 		</form>
 		<div class="mt-4">
 			{#if output}
-				<WidgetOutputTableQA {output} />
+				<WidgetOutputTableQA {output} {isAnswerOnlyOutput} />
 			{/if}
 			{#if table.length > 1 || table[0].length > 1}
 				<WidgetTableInput {highlighted} onChange={onChangeTable} {table} />
