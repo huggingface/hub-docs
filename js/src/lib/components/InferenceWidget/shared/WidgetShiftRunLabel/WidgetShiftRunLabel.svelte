98912c12035290ba318c6f3e406573cfdabeb2c8
Mishig Davaadorj <dmishig@gmail.com>
2022-06-27 13:18:18 +0200

    fix css

--- js/src/lib/components/InferenceWidget/shared/WidgetShiftRunLabel/WidgetShiftRunLabel.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetShiftRunLabel/WidgetShiftRunLabel.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div
-	class="flex items-center space-x-1 mt-16 {isLoading
+	class="no-hover:hidden flex items-center space-x-1 mt-16 {isLoading
 		? 'opacity-40'
 		: 'opacity-70'}"
 >
-	<span class="no-hover:hidden text-xs border-2 py-1 px-1.5 rounded-lg"
+	<span class="text-xs border-2 py-1 px-1.5 rounded-lg"
 		>Shift + Enter</span
 	>
 	{#if isLoading}
