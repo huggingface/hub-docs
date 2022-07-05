7ec2924a4ccb5aaced689191d07027b75db69d01
Mishig Davaadorj <dmishig@gmail.com>
2022-07-05 12:36:18 +0200

    Timer always show in seconds

--- js/src/lib/components/InferenceWidget/shared/WidgetTimer/WidgetTimer.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetTimer/WidgetTimer.svelte
@@ -5,14 +5,10 @@
 	let interval: ReturnType<typeof setInterval>;
 	let shouldDisplay = false;
 
-	$: counterHuman =
-		Math.floor(counterSeconds / 60) > 0
-			? `${Math.floor(counterSeconds / 60)} : ${(
-					counterSeconds % 60
-			  ).toLocaleString(undefined, { minimumFractionDigits: 1 })}`
-			: (counterSeconds % 60).toLocaleString(undefined, {
-					minimumFractionDigits: 1,
-			  });
+	// timer show in seconds
+	$: counterHuman = counterSeconds.toLocaleString(undefined, {
+		minimumFractionDigits: 1,
+	});
 
 	export function start() {
 		// reset timer for new run
@@ -33,5 +29,5 @@
 </script>
 
 {#if shouldDisplay}
-	<span class="opacity-80">{counterHuman}</span>
+	<span class="opacity-80">{counterHuman}s</span>
 {/if}
