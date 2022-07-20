a234470c16d3c55732d52105193485da61db59a2
Mishig Davaadorj <dmishig@gmail.com>
2022-07-20 10:27:38 +0100

    Fix `WidgetTextArea` (#251)

--- js/src/lib/components/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
@@ -9,8 +9,6 @@
 	export let isLoading = false;
 	export let size: "small" | "big" = "small";
 
-	let newValue = "";
-	let isOnFreshLine = true;
 	let containerSpanEl: HTMLSpanElement;
 	const typingEffectSpeedMs = 12;
 	const classNamesInput = "font-normal text-black dark:text-white";
@@ -64,42 +62,7 @@
 	}
 
 	function updateInnerTextValue() {
-		newValue = "";
-		isOnFreshLine = true;
-		parseChildNodesForValueAndLines(containerSpanEl.childNodes);
-		value = newValue;
-	}
-
-	// from https://stephenhaney.com/2020/get-contenteditable-plaintext-with-correct-linebreaks/
-	// Recursive function to navigate childNodes and build linebreaks with text
-	function parseChildNodesForValueAndLines(childNodes: NodeListOf<ChildNode>) {
-		for (let i = 0; i < childNodes.length; i++) {
-			const childNode = childNodes[i];
-
-			if (childNode.nodeName === "BR") {
-				// BRs are always line breaks which means the next loop is on a fresh line
-				newValue += "\n";
-				isOnFreshLine = true;
-				continue;
-			}
-
-			// We may or may not need to create a new line
-			if (childNode.nodeName === "DIV" && isOnFreshLine === false) {
-				// Divs create new lines for themselves if they aren't already on one
-				newValue += "\n";
-			}
-
-			// Whether we created a new line or not, we'll use it for this content so the next loop will not be on a fresh line:
-			isOnFreshLine = false;
-
-			// Add the text content if this is a text node:
-			if (childNode.nodeType === 3 && childNode.textContent) {
-				newValue += childNode.textContent;
-			}
-
-			// If this node has children, get into them as well:
-			parseChildNodesForValueAndLines(childNode.childNodes);
-		}
+		value = containerSpanEl?.textContent ?? "";
 	}
 
 	export function setValue(text: string) {
@@ -110,12 +73,13 @@
 
 <WidgetLabel {label}>
 	<svelte:fragment slot="after">
+		<!-- `whitespace-pre-wrap inline-block` are needed to get correct newlines from `el.textContent` on Chrome -->
 		<span
 			class="{isLoading ? 'pointer-events-none' : ''} {label
 				? 'mt-1.5'
 				: ''} block overflow-auto resize-y py-2 px-3 w-full {size === 'small'
 				? 'min-h-[42px]'
-				: 'min-h-[144px]'} max-h-[500px] whitespace-pre-wrap border border-gray-200 rounded-lg shadow-inner outline-none focus:ring focus:ring-blue-200 focus:shadow-inner dark:bg-gray-925"
+				: 'min-h-[144px]'} max-h-[500px] whitespace-pre-wrap inline-block border-gray-200 rounded-lg shadow-inner outline-none focus:ring focus:ring-blue-200 focus:shadow-inner dark:bg-gray-925"
 			role="textbox"
 			contenteditable
 			style="--placeholder: '{placeholder}'"
@@ -123,7 +87,7 @@
 			dir="auto"
 			bind:this={containerSpanEl}
 			on:paste|preventDefault={handlePaste}
-			on:keypress={updateInnerTextValue}
+			on:input={updateInnerTextValue}
 		/>
 	</svelte:fragment>
 </WidgetLabel>
