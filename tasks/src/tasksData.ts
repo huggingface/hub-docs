7f3b52ca2785a7da59461a610a0a5bf9816af4b7
Thibault Goehringer <tgoehringer@gmail.com>
2022-09-29 17:07:39 +0200

    Reorder import

--- tasks/src/tasksData.ts
+++ tasks/src/tasksData.ts
@@ -11,8 +11,8 @@ import objectDetection from "./object-detection/data";
 import questionAnswering from "./question-answering/data";
 import sentenceSimilarity from "./sentence-similarity/data";
 import summarization from "./summarization/data";
-import textToSpeech from "./text-to-speech/data";
 import tabularClassification from "./tabular-classification/data"
+import textToSpeech from "./text-to-speech/data";
 import tokenClassification from "./token-classification/data";
 import translation from "./translation/data";
 import textClassification from "./text-classification/data";
