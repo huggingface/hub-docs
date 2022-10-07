4f0f718e9bd5f7f839453391ffc6e1d04e389864
JuanCarlosPi <juancopi_81@hotmail.com>
2022-10-07 08:10:05 -0500

    Update tasks/src/unconditional-image-generation/data.ts
    Removed widgetModels as there is no widget for them as for now!
    
    Co-authored-by: Merve Noyan <merveenoyan@gmail.com>

--- tasks/src/unconditional-image-generation/data.ts
+++ tasks/src/unconditional-image-generation/data.ts
@@ -47,7 +47,7 @@ const taskData: TaskDataCustom = {
 		},
 	],
 	summary:      "Unconditional image generation is the task of generating images with no condition in any context (prompt text, another image, etc.). Once trained, the model will create images that resemble its training data distribution.",
-	widgetModels: ["google/vit-base-patch16-224"],
+	widgetModels: "",
 	// TODO: Add related video
 	youtubeId:    "",
 };
