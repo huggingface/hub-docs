04ad7db2504d4534241656cd15ceae9e567bf514
Shivshankar Shukla <76114538+02shanks@users.noreply.github.com>
2022-10-07 17:00:53 +0530

    Updated data.ts

--- tasks/src/Reinforcement-learning/data.ts
+++ tasks/src/Reinforcement-learning/data.ts
@@ -3,75 +3,28 @@ import type { TaskDataCustom } from "../Types";
 const taskData: TaskDataCustom = {
 	datasets: [
 		{
-			description: "A widely used dataset used to benchmark multiple variants of text classification.",
-			id:          "glue",
+			description: "",
+			id:          "",
 		},
 		{
-			description: "A text classification dataset used to benchmark natural language inference models",
-			id:          "snli",
+			description: "",
+			id:          "",
 		},
 	],
-	demo: {
-		inputs: [
-			{
-				label:   "Input",
-				content: "I love Hugging Face!",
-				type:    "text",
-			},
-			
-		],
-		outputs: [
-			{
-				type: "chart",
-				data: [
-					{
-						label: "POSITIVE",
-						score: 0.90,
-					},
-					{
-						label: "NEUTRAL",
-						score: 0.10,
-					},
-					{
-						label: "NEGATIVE",
-						score: 0.00,
-					},
-				],
-			},
-		],
-	},
-	metrics: [
-		{
-			description: "",
-			id:          "accuracy",
-		},
-		{
-			description: "",
-			id:          "recall",
-            
-		},
-		{
-			description: "",
-			id:          "precision",
-		},
-		{
-			description: "The F1 metric is the harmonic mean of the precision and recall. It can be calculated as: F1 = 2 * (precision * recall) / (precision + recall)",
-			id:          "f1",
-		},
-	],
+
 	models: [
 		{
-			description: "A robust model trained for sentiment analysis.",
-			id:          "distilbert-base-uncased-finetuned-sst-2-english",
+			description: "",
+			id:          "",
 		},
 		{
-			description: "Multi-genre natural language inference model.",
-			id:          "roberta-large-mnli",
+			description: "",
+			id:          "",
 		},
 	],
-	summary:      "Text Classification is the task of assigning a label or class to a given text. Some use cases are sentiment analysis, natural language inference, and assessing grammatical correctness.",
-	widgetModels: ["distilbert-base-uncased-finetuned-sst-2-english"],
-	youtubeId:    "leNG9fN9FQU",
+	summary:      "",
+	widgetModels: [""],
+	youtubeId:    "",
 };
 
 export default taskData;
