c47de8ef997e1d7565c7923b1cce76568d7d39f6
Oğuz Akif Tüfekcioğlu <oguztufek@gmail.com>
2022-10-07 17:13:47 +0300

    General purpose model and general purpose datasets are added

--- tasks/src/text-to-image/data.ts
+++ tasks/src/text-to-image/data.ts
@@ -3,12 +3,12 @@ import type { TaskDataCustom } from "../Types";
 const taskData: TaskDataCustom = {
     datasets: [
         {
-            description: "Dataset used to train Pokémon text to image model.",
-            id: "lambdalabs/pokemon-blip-captions",
+            description: "A large-scale image dataset containing images of everyday objects and people with their textual descriptions.",
+            id: "ChristophSchuhmann/MS_COCO_2017_URL_TEXT",
         },
         {
-            description: "A dataset that contains more than 30K images with their corresponding text from the Shahnameh.",
-            id: "sadrasabouri/ShahNegar",
+            description: "A dataset created from Midjourney User Prompts & Generated Images",
+            id: "nateraw/midjourney-texttoimage-new",
         },
     ],
     demo: {
@@ -18,12 +18,12 @@ const taskData: TaskDataCustom = {
     metrics: [],
     models: [
         {
-            description: "A latent text-to-image diffusion model capable of generating photo-realistic images given any text input.",
-            id: "CompVis/stable-diffusion-v1-4",
+            description: "A model that can be used to generate images based on text prompts. The DALL·E Mega model is the largest version of DALLE Mini.",
+            id: "dalle-mini/dalle-mega",
         },
         {
-            description: "A latent text-to-image diffusion model that has been conditioned on high-quality anime images through fine-tuning.",
-            id: "hakurei/waifu-diffusion",
+            description: "A latent text-to-image diffusion model capable of generating photo-realistic images given any text input.",
+            id: "CompVis/stable-diffusion-v1-4",
         },
     ],
     summary: "Generating image from input text. These models that can be used to generate and modify images based on text prompts.",
