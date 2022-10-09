a937669f743e7bca094afa882f86e4e012ba5851
jlondonob <j.londonobo@gmail.com>
2022-10-08 22:56:40 -0500

    ♻️ Unify style and formatting

--- tasks/src/visual-question-answering/data.ts
+++ tasks/src/visual-question-answering/data.ts
@@ -2,8 +2,8 @@ import { type TaskDataCustom } from "../Types";
 
 const taskData: TaskDataCustom = {
 	datasets: [],
-	demo:     {
-		inputs:  [
+	demo: {
+		inputs: [
 			{
 				filename: "elephant.jpeg",
 				type:     "img",
@@ -35,25 +35,25 @@ const taskData: TaskDataCustom = {
 		],
 	},
 	isPlaceholder: false,
-	metrics:       [
+	metrics: [
 		{
 			description: "",
 			id:          "accuracy",
 		},
 		{
 			description: "Measures how much a predicted answer differs from the ground truth based on the difference in their semantic meaning.",
-			id:          "Wu-Palmer Similarity",
+			id:          "wu-palmer similarity",
 		},
 	],
-	models:        [
+	models: [
 		{
 			description: "Robust Visual Question Answering model trained on the VQAv2 dataset.",
 			id:          "dandelin/vilt-b32-finetuned-vqa",
 		},
 	],
-	summary:       "Visual Question Answering is the task of answering open-ended questions based on an image. They output natural language responses to natural language questions.",
-	widgetModels:  ["dandelin/vilt-b32-finetuned-vqa"],
-	youtubeId:     "",
+	summary:      "Visual Question Answering is the task of answering open-ended questions based on an image. They output natural language responses to natural language questions.",
+	widgetModels: ["dandelin/vilt-b32-finetuned-vqa"],
+	youtubeId:    "",
 };
 
 export default taskData;
