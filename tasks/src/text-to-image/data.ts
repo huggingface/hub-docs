51ec3542cd44a8d66c694c03582fe1649e31030b
Oğuz Akif Tüfekcioğlu <oguztufek@gmail.com>
2022-10-10 17:14:51 +0300

    indent errors are fixed

--- tasks/src/text-to-image/data.ts
+++ tasks/src/text-to-image/data.ts
@@ -1,34 +1,34 @@
 import type { TaskDataCustom } from "../Types";
 
 const taskData: TaskDataCustom = {
-    datasets: [
+    datasets:      [
         {
             description: "A large-scale image dataset containing images of everyday objects and people with their textual descriptions.",
-            id: "ChristophSchuhmann/MS_COCO_2017_URL_TEXT",
+            id:          "ChristophSchuhmann/MS_COCO_2017_URL_TEXT",
         },
         {
             description: "A dataset created from Midjourney User Prompts & Generated Images",
-            id: "nateraw/midjourney-texttoimage-new",
+            id:          "nateraw/midjourney-texttoimage-new",
         },
     ],
-    demo: {
-        inputs: [],
-        outputs: [],
-    },
-    metrics: [],
-    models: [
+    demo:          {
+		inputs:  [],
+		outputs: [],
+	},
+    metrics:       [],
+    models:        [
         {
             description: "A model that can be used to generate images based on text prompts. The DALL·E Mega model is the largest version of DALLE Mini.",
-            id: "dalle-mini/dalle-mega",
+            id:          "dalle-mini/dalle-mega",
         },
         {
             description: "A latent text-to-image diffusion model capable of generating photo-realistic images given any text input.",
-            id: "CompVis/stable-diffusion-v1-4",
+            id:          "CompVis/stable-diffusion-v1-4",
         },
     ],
-    summary: "Generating image from input text. These models that can be used to generate and modify images based on text prompts.",
+    summary:      "Generating image from input text. These models that can be used to generate and modify images based on text prompts.",
     widgetModels: [],
-    youtubeId: "",
+    youtubeId:    "",
 };
 
 export default taskData;
