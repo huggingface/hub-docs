b5c3391bcec39ba4555582c634444e47b811ad26
SHIV-anna <shivshankar07123@gmail.com>
2022-10-10 22:39:57 +0530

    Added changes

---
+++ tasks/src/Zero-Shot_Image_Classification/data.ts
@@ -0,0 +1,30 @@
+import type { TaskDataCustom } from "../Types";
+
+const taskData: TaskDataCustom = {
+	datasets: [
+		{
+			description: "",
+			id:          "",
+		},
+		{
+			description: "",
+			id:          "",
+		},
+	],
+	
+	models: [
+		{
+			description: "",
+			id:          "",
+		},
+		{
+			description: "",
+			id:          "",
+		},
+	],
+	summary:      "Zero-shot image classification is a task in computer vision where the aim is to classify images into one of several pre-defined categories, without any prior training or knowledge of the categories.",
+	widgetModels: [""],
+	youtubeId:    "",
+};
+
+export default taskData;
