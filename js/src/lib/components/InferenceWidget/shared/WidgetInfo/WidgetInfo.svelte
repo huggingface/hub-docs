8d8852b11a3e1f5a2c9594bf3962461f4ff2c701
Victor Muštar <victor.mustar@gmail.com>
2022-10-13 17:31:11 +0200

    Bloom: add AzureML to the widget (#404)
    * add powered by azureml
    
    * wording to reduce width

--- js/src/lib/components/InferenceWidget/shared/WidgetInfo/WidgetInfo.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetInfo/WidgetInfo.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import type { LoadingStatus } from "../types";
+	import type { WidgetProps, LoadingStatus } from "../types";
 
+	import IconAzureML from "../../../Icons/IconAzureML.svelte";
+
+	export let model: WidgetProps["model"];
 	export let computeTime: string;
 	export let error: string;
 	export let modelStatus: LoadingStatus = "unknown";
@@ -10,13 +13,36 @@
 		loaded: "This model is currently loaded and running on the Inference API.",
 		unknown: "This model can be loaded on the Inference API on-demand.",
 	} as const;
+
+	const azureStatus = {
+		error: "⚠️ This model could not be loaded.",
+		loaded: "This model is loaded and running on AzureML Managed Endpoint",
+		unknown: "This model can be loaded loaded on AzureML Managed Endpoint",
+	} as const;
 </script>
 
 <div class="mt-2">
 	<div class="text-gray-400 text-xs">
 		{#if computeTime}
 			Computation time on cpu: {computeTime}
-		{:else if modelStatus}
+		{:else if model.id === "bigscience/bloom"}
+			<div class="flex items-baseline">
+				<div class="flex items-center whitespace-nowrap text-gray-700">
+					<IconAzureML classNames="mr-1 flex-none" /> Powered by&nbsp;
+					<a
+						class="underline hover:text-gray-800"
+						href="https://azure.microsoft.com/products/machine-learning"
+						target="_blank">AzureML</a
+					>
+				</div>
+				<div
+					class="flex border-dotter border-b border-gray-100 flex-1 mx-2 -translate-y-px"
+				/>
+				<div>
+					{azureStatus[modelStatus]}
+				</div>
+			</div>
+		{:else}
 			{status[modelStatus]}
 		{/if}
 	</div>
