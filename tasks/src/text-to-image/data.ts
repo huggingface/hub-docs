b96d4486b6f8e0a5ec49b34c9b544bc3c7ee5ef3
samyak1999 <samyakSKS1999@gmail.com>
2022-10-14 20:48:22 +0530

    Change in datasets,metrics and summary

--- tasks/src/text-to-image/data.ts
+++ tasks/src/text-to-image/data.ts
@@ -4,44 +4,19 @@ const taskData: TaskDataCustom = {
 	datasets: [
 		{
 			// TODO write proper description
-			description: "Caltech-UCSD Birds-200-2011 is an extended version of the CUB-200 dataset, with roughly double the number of images per class and new part location annotations.",
-			id:          "CUB-200-2011",
+			description: "12M image-text pairs were extracted from Reddit creating a large-scale dataset called RedCaps. Reddit images and descriptions portray and describe a wide range of objects and scenes",
+			id:          "red_caps",
 		},
 		{
 			// TODO write proper description
-			description: "Oxford-102 Flower is a 102 category dataset, consisting of 102 flower categories. The flowers are chosen to be flower commonly occurring in the United Kingdom. The images have large scale, pose and light variations.",
-			id:          "Oxford-102 Flower",
-
+			description: "Conceptual Captions is a dataset consisting of 3.3M images annotated with captions",
+			id: "conceptual_captions"
 		},
 		{
-			description:"COCO is a large-scale object detection, segmentation, and captioning dataset.",
-			id: "MS-COCO"
+			description:"Conceptual 12M (CC12M) is a dataset with 12 million image-text pairs specifically meant to be used for image-text cross-modal pre-training",
+			id: "conceptual_12M"
 		},
-		{
-			description:  "Multi-Modal-CelebA-HQ is a large-scale face image dataset for text-to-image-generation, text-guided image manipulation, sketch-to-image generation, GANs for face generation and editing, image caption, and VQA",
-			id: "Multi_Modal_Celeba_HQ"
-		},
-		{
-			description:  "CelebA-Dialog is a large-scale visual-language face dataset. It has two properties: (1) Facial images are annotated with rich fine-grained labels, which classify one attribute into multiple degrees according to its semantic meaning. (2) Accompanied with each image, there are captions describing the attributes and a user request sample.",
-			id : 'CelebA_Dialog'
-		},
-		{
-			description:  "FFHQ-Text is a small-scale face image dataset with large-scale facial attributes, designed for text-to-face generation & manipulation, text-guided facial image manipulation, and other vision-related tasks",
-			id : "FFHQ-Text"
-		},
-		{
-			description:"CelebAText-HQ is a large-scale face image dataset with large-scale facial attributes, designed for text-to-face generation",
-			id: 'CelebAText-HQ'
-		},
-		{
-			description:"CelebA-Dialog is a large-scale visual-language face dataset with the following features. Facial images are annotated with rich fine-grained labels, which classify one attribute into multiple degrees according to its semantic meaning. Accompanied with each image, there are textual captions describing the attributes and a user editing request sample",
-			id :'CelebA-Dialog'
-		},
-		{
-			description:'DeepFashion-MultiModal is a large-scale high-quality human dataset with rich multi-modal annotations.',
-			id: 'DeepFashion-MultiModal'
-		}
-
+		
 	],
 	demo: {
 		inputs: [],
@@ -49,23 +24,16 @@ const taskData: TaskDataCustom = {
 	},
 	metrics: [
 		{
-			description: "The inception score involves using a pre-trained deep learning neural network model for image classification to classify the generated images.",
+			description: "The inception score captures two properties of a collection of generated (image quality and diversity). The inception score is calculated by first using a pre-trained Inception v3 model to predict the class probabilities for each generated image. ",
 			id:          "IS",
 		},
 		{
-			description: "The Fréchet inception distance (FID) is a metric used to assess the quality of images created by a generative model, like a generative adversarial network (GAN).",
+			description: "The Fréchet inception distance (FID) is a metric that calculates the distance between the feature vectors of real images and the feature vectors of fake images",
 			id:          "FID",
 		},
+	
 		{
-			description: "R-precision is defined as the proportion of the top-R retrieved documents that are relevant, where R is the number of relevant documents for the current query.",
-			id:          "R-precision",
-		},
-		{
-			description: "L2 loss, also known as Squared Error Loss, is the squared difference between a prediction and the actual value, calculated for each example in a dataset",
-			id:          "L2",
-		},
-		{
-			description:"'Learned Perceptual Image Patch Similarity' evaluates the distance between image patches. Higher means further/more different. Lower means more similar.",
+			description:"'Learned Perceptual Image Patch Similarity'is used to judge the perceptual similarity between two images. A low LPIPS score means that image patches are perceptual similar. ",
 			id:			 "LPIPS"
 		},
 
@@ -73,14 +41,14 @@ const taskData: TaskDataCustom = {
 	models: [
 		{
 			// TO DO: write description
-			description: '',
-			id:          ''
+			description: 'Imagen, a text-to-image diffusion model with an unprecedented degree of photorealism and a deep level of language understanding.',
+			id:          'imagen'
 		},
 		
 	],
-	summary:      "Deep learning research has achieved several significant technological advances in the domains of Computer Vision (CV) and Natural Language Processing (NLP) over the past few decades. In these hitherto distinct domains, researchers now seem interested in fusing semantic and visual information. Numerous studies have been done on text-to-image synthesis methods, which convert the textual description (keywords or sentences) that is input into realistic visuals",
+	summary:      "Because of the advancement of Deep Learning technologies, we can generate anything we want that also invloves automatic synthesis of realistic images from text or text-to-image synthesis. ",
 	widgetModels: [],
-	youtubeId:    "SVcsDDABEkM",
+	youtubeId:    "",
 };
 
 export default taskData;
