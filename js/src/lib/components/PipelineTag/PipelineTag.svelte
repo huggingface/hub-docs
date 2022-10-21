ecb4bb4af030f850f37ae38541e0ca7c0d755f18
Julien Chaumond <julien@huggingface.co>
2022-10-21 13:27:46 +0200

    `ModelPipelineIcon` => `PipelineIcon` (#441)
    * `ModelPipelineIcon` => `PipelineIcon`
    
    * tweak

--- js/src/lib/components/PipelineTag/PipelineTag.svelte
+++ js/src/lib/components/PipelineTag/PipelineTag.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { PIPELINE_DATA } from "../../interfaces/Types";
-	import ModelPipelineIcon from "../ModelPipelineIcon/ModelPipelineIcon.svelte";
+	import PipelineIcon from "../PipelineIcon/PipelineIcon.svelte";
 
 	export let classNames = "";
 	export let pipeline = "";
 </script>
 
 <div class="inline-flex items-center {classNames}">
-	<ModelPipelineIcon classNames="mr-1" {pipeline} />
+	<PipelineIcon classNames="mr-1" {pipeline} />
 	<span>
 		{PIPELINE_DATA[pipeline].name ?? pipeline}
 	</span>
