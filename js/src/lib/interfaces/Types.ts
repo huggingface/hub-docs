ec705d08683a2b7171f219342cf6f1fca35fc534
Julien Chaumond <julien@huggingface.co>
2022-10-27 22:02:46 +0200

    Micro-optimization to make @coyotte508 happy 😂 (#455)

--- js/src/lib/interfaces/Types.ts
+++ js/src/lib/interfaces/Types.ts
@@ -607,9 +607,11 @@ export const PIPELINE_DATA = ensureRecordOfPipelines({
 
 export type PipelineType = keyof typeof PIPELINE_DATA;
 export const ALL_PIPELINE_TYPES = Object.keys(PIPELINE_DATA) as PipelineType[];
+export const ALL_PIPELINE_TYPES_SET = new Set(ALL_PIPELINE_TYPES);
 
 export const ALL_SUBTASKS = Object.values(PIPELINE_DATA).flatMap(data => data.subtasks ?? []);
 export const ALL_SUBTASK_TYPES = ALL_SUBTASKS.map(s => s.type);
+export const ALL_SUBTASK_TYPES_SET = new Set(ALL_SUBTASK_TYPES);
 
 /*
  * Specification of pipeline tag display order.
