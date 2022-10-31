6b8d11179faa1c77afc14617fb5656c4326b5ef1
Shivshankar Shukla <76114538+02shanks@users.noreply.github.com>
2022-10-31 18:45:43 +0530

    Added task description and use cases for zero-shot-classification (#386)
    Co-authored-by: Merve Noyan <merveenoyan@gmail.com>

---
+++ tasks/src/zero-shot-classification/data.ts
@@ -0,0 +1,64 @@
+import type { TaskDataCustom } from "../Types";
+
+const taskData: TaskDataCustom = {
+	datasets: [
+		{
+			description: "",
+			id:          "",
+		},
+		{
+			description: "",
+			id:          "",
+		},
+	],
+	demo: {
+		inputs: [
+			{
+			},
+			
+		],
+		outputs: [
+			{
+				type: "",
+				data: [
+
+				],
+			},
+		],
+	},
+	metrics: [
+		{
+			description: "",
+			id:          "",
+		},
+		{
+			description: "",
+			id:          "",
+            
+		},
+		{
+			description: "",
+			id:          "",
+		},
+		{
+			description: "",
+			id:          "",
+		},
+	],
+	
+	models: [
+		{
+			description: "",
+			id:          "",
+		},
+		{
+			description: "",
+			id:          "",
+		},
+	],
+	summary:      "Zero-shot text classification is a task in natural language processing (NLP) where a model is trained on a set of labeled examples but is then able to classify new examples from previously unseen classes by transferring knowledge from seen to unseen classes.",
+	widgetModels: [""],
+	youtubeId:    "",
+};
+
+export default taskData;
