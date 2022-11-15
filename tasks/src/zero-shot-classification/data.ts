3dc5c8016e315d9339c774a706c0bb3726226bb5
Merve Noyan <merveenoyan@gmail.com>
2022-11-15 13:52:33 +0100

    fixed data.ts

--- tasks/src/zero-shot-classification/data.ts
+++ tasks/src/zero-shot-classification/data.ts
@@ -1,19 +1,14 @@
 import type { TaskDataCustom } from "../Types";
 
 const taskData: TaskDataCustom = {
-	datasets: [
-	],
+	datasets: [],
 	demo: {
-		inputs: [
-		],
-		outputs: [
-		],
+		inputs: [],
+		outputs: [],
 	},
-	metrics: [
-	],
+	metrics: [],
 	
-	models: [
-	],
+	models: [],
 	summary:      "Zero-shot text classification is a task in natural language processing (NLP) where a model is trained on a set of labeled examples but is then able to classify new examples from previously unseen classes by transferring knowledge from seen to unseen classes.",
 	widgetModels: [],
 };
