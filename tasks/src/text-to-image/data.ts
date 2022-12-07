7a742b51159e4f78db8ea23c55f82c6c36362c92
Merve Noyan <merveenoyan@gmail.com>
2022-12-07 10:47:21 +0300

    widget for text-to-image task

--- tasks/src/text-to-image/data.ts
+++ tasks/src/text-to-image/data.ts
@@ -38,7 +38,7 @@ const taskData: TaskDataCustom = {
         },
     ],
     summary:      "Generates images from input text. These models can be used to generate and modify images based on text prompts.",
-    widgetModels: [],
+    widgetModels: ["CompVis/stable-diffusion-v1-4"],
     youtubeId:    "",
 };
 
