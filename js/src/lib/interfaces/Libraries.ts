2e4d048514b86f5822ad4338dac62378a8d4eb7d
Merve Noyan <merveenoyan@gmail.com>
2023-01-05 09:07:19 +0300

    Added new file format param to skops snippet script (#417)
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>

--- js/src/lib/interfaces/Libraries.ts
+++ js/src/lib/interfaces/Libraries.ts
@@ -161,9 +161,9 @@ const paddlenlp = (model: ModelData) => {
 	return [
 	  `from paddlenlp.transformers import AutoModel, AutoTokenizer`,
 	  "",
-	  `tokenizer = AutoTokenizer.from_pretrained("${model.id}"${model.private ? ", use_auth_token=True" : ""}, from_hf_hub=True)`, 
+	  `tokenizer = AutoTokenizer.from_pretrained("${model.id}"${model.private ? ", use_auth_token=True" : ""}, from_hf_hub=True)`,
 	  `model = AutoModel.from_pretrained("${model.id}"${model.private ? ", use_auth_token=True" : ""}, from_hf_hub=True)`,
-        ].join("\n");
+	].join("\n");
 };
 
 const pyannote_audio_pipeline = (model: ModelData) =>
@@ -240,20 +240,32 @@ model = timm.create_model("hf_hub:${model.id}", pretrained=True)`;
 const sklearn = (model: ModelData) => {
 	if (model.tags?.includes("skops")) {
 		const skopsmodelFile = model.config?.sklearn?.filename;
-		return `from skops.hub_utils import download
+		const skopssaveFormat = model.config?.sklearn?.model_format;
+		if (skopssaveFormat === "pickle") {
+			return `import joblib
+from skops.hub_utils import download
+download("${model.id}", "path_to_folder")
+model = joblib.load(
+	"${skopsmodelFile}"
+)
+# only load pickle files from sources you trust
+# read more about it here https://skops.readthedocs.io/en/stable/persistence.html`;
+		} else {
+			return `from skops.hub_utils import download
 from skops.io import load
-
 download("${model.id}", "path_to_folder")
 # make sure model file is in skops format
 # if model is a pickle file, make sure it's from a source you trust
 model = load("path_to_folder/${skopsmodelFile}")`;
+		}
 	} else {
 		return `from huggingface_hub import hf_hub_download
 import joblib
-
 model = joblib.load(
 	hf_hub_download("${model.id}", "sklearn_model.joblib")
-	)`;
+)
+# only load pickle files from sources you trust
+# read more about it here https://skops.readthedocs.io/en/stable/persistence.html`;
 	}
 };
 
