cb7d2b0072f178e899d4f3cb7a214a9bc8b686dd
Sijun He <sijun.he@hotmail.com>
2023-01-06 13:17:39 +0800

    auto_model

--- js/src/lib/interfaces/Libraries.ts
+++ js/src/lib/interfaces/Libraries.ts
@@ -163,10 +163,10 @@ const paddlenlp = (model: ModelData) => {
 		return `# ⚠️ Type of model unknown`;
 	} else {
 		return [
-			`from transformers import AutoTokenizer, ${info.auto_model}`,
+			`from paddlenlp.transformers import AutoTokenizer, ${info.auto_model}`,
 			"",
 			`tokenizer = AutoTokenizer.from_pretrained("${model.id}"${model.private ? ", use_auth_token=True" : ""}, from_hf_hub=True)`,
-			`model = AutoModel.from_pretrained("${model.id}"${model.private ? ", use_auth_token=True" : ""}, from_hf_hub=True)`,
+			`model = ${info.auto_model}.from_pretrained("${model.id}"${model.private ? ", use_auth_token=True" : ""}, from_hf_hub=True)`,
 		].join("\n");
 	}
 };
