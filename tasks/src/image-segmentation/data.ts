0b2d602ab744ed0acae7d4b23581373cacf09a01
Rashmi Banthia <rjain29@gmail.com>
2023-01-11 08:25:53 -0500

    added instance segmentation task spaces

--- tasks/src/image-segmentation/data.ts
+++ tasks/src/image-segmentation/data.ts
@@ -59,6 +59,10 @@ const taskData: TaskDataCustom = {
 			description: "A semantic segmentation application that predicts human silhouettes.",
 			id:          "keras-io/Human-Part-Segmentation",
 		},
+		{
+			description: "Cell Instance Segmentation - Sartorius Kaggle Competition",
+			id:          "rashmi/sartorius-cell-instance-segmentation",
+		},
 	],
 	summary:      "Image Segmentation divides an image into segments where each pixel in the image is mapped to an object. This task has multiple variants such as instance segmentation, panoptic segmentation and semantic segmentation.",
 	widgetModels: ["facebook/detr-resnet-50-panoptic"],
