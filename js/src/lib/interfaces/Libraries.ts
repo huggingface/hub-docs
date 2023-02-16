3982fa279d2b2af38117e41a65f0a0a440cb2251
Tiezhen WANG <38108242+xianbaoqian@users.noreply.github.com>
2023-02-16 10:50:19 +0800

    Fix breakage in hub-docs due to unknown fields

--- js/src/lib/interfaces/Libraries.ts
+++ js/src/lib/interfaces/Libraries.ts
@@ -296,20 +296,11 @@ const fastai = (model: ModelData) =>
 learn = from_pretrained_fastai("${model.id}")`;
 
 const mindspore = (model: ModelData) => {
-	const architecture = model.config.architectures[0].toLowerCase();
-	if (model.tags?.includes("image-classification")){
-		const class_num = model.config.class_num[0];
-		return `from tinyms.model import Model, ${architecture}
-from huggingface_hub import hf_hub_download
-net = ${architecture}(class_num=${class_num}, is_training=False)
-ms_model = Model(net)
-ms_model.load_checkpoint(hf_hub_download("${model.id}", "mindspore_model.ckpt"))`
-	}
 	return mindsporeUnknown();
 };
 
 const mindsporeUnknown = () =>
-	`unknown model task (must be image-classification)`;
+	`Not supported`;
 
 const sampleFactory = (model: ModelData) =>
 	`python -m sample_factory.huggingface.load_from_hub -r ${model.id} -d ./train_dir`;
