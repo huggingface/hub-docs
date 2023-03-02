9a6c76127b5fa54d397cffbafc5726153db7d727
Merve Noyan <merveenoyan@gmail.com>
2023-03-02 18:10:31 +0100

    Added description to image-to-text (#702)
    * Added description
    
    * Update tasks/src/image-to-text/data.ts
    
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>
    
    ---------
    
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>

--- tasks/src/image-to-text/data.ts
+++ tasks/src/image-to-text/data.ts
@@ -67,7 +67,7 @@ const taskData: TaskDataCustom = {
 			id:          "johko/capdec-image-captioning",
 		},
 	],
-	summary:      "",
+	summary:      "Image to text models output a text from a given image. Image captioning or optical character recognition can be considered as the most common applications of image to text.",
 	widgetModels: ["nlpconnect/vit-gpt2-image-captioning"],
 	youtubeId:    "",
 };
