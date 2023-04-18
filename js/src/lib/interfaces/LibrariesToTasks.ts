a75565b100805dfb0108b6176211f1a6105674cb
Omar Sanseviero <osanseviero@gmail.com>
2023-04-18 19:26:22 +0200

    Add OpenCLIP to inference API (#748)

--- js/src/lib/interfaces/LibrariesToTasks.ts
+++ js/src/lib/interfaces/LibrariesToTasks.ts
@@ -54,6 +54,10 @@ export const LIBRARY_TASK_MAPPING_EXCLUDING_TRANSFORMERS: Partial<Record<ModelLi
 	"nemo": [
 		"automatic-speech-recognition",
 	],
+	"open_clip": [
+		"zero-shot-classification",
+		"zero-shot-image-classification",
+	],
 	"paddlenlp": [
 		"conversational",
 		"fill-mask",
