700ad7b53bab740530401f05f25bef2c5cd10fca
Mishig <dmishig@gmail.com>
2023-05-01 17:47:42 +0200

    Img-to-img-widget (#760)
    * Implement ImageToImageWidget
    
    * Add widget example
    
    * lint

---
+++ js/src/lib/components/InferenceWidget/widgets/ImageToImageWidget/ImageToImageWidget.svelte
@@ -0,0 +1,221 @@
+<script lang="ts">
+	import type { WidgetProps } from "../../shared/types";
+
+	import WidgetFileInput from "../../shared/WidgetFileInput/WidgetFileInput.svelte";
+	import WidgetDropzone from "../../shared/WidgetDropzone/WidgetDropzone.svelte";
+	import WidgetTextInput from "../../shared/WidgetTextInput/WidgetTextInput.svelte";
+	import WidgetSubmitBtn from "../../shared/WidgetSubmitBtn/WidgetSubmitBtn.svelte";
+	import WidgetWrapper from "../../shared/WidgetWrapper/WidgetWrapper.svelte";
+	import {
+		addInferenceParameters,
+		getDemoInputs,
+		getResponse,
+	} from "../../shared/helpers";
+	import { onMount } from "svelte";
+
+	export let apiToken: WidgetProps["apiToken"];
+	export let apiUrl: WidgetProps["apiUrl"];
+	export let callApiOnMount: WidgetProps["callApiOnMount"];
+	export let model: WidgetProps["model"];
+	export let noTitle: WidgetProps["noTitle"];
+	export let includeCredentials: WidgetProps["includeCredentials"];
+
+	let computeTime = "";
+	let error: string = "";
+	let isLoading = false;
+	let modelLoading = {
+		isLoading: false,
+		estimatedTime: 0,
+	};
+	let output = "";
+	let outputJson = "";
+	let prompt = "";
+	let imgSrc = "";
+	let imageBase64 = "";
+
+	async function onSelectFile(file: File | Blob) {
+		imgSrc = URL.createObjectURL(file);
+		await updateImageBase64(file);
+	}
+
+	function updateImageBase64(file: File | Blob): Promise<void> {
+		return new Promise((resolve, reject) => {
+			let fileReader: FileReader = new FileReader();
+			fileReader.onload = async () => {
+				try {
+					const imageBase64WithPrefix: string = fileReader.result as string;
+					imageBase64 = imageBase64WithPrefix.split(",")[1]; // remove prefix
+					isLoading = false;
+					resolve();
+				} catch (err) {
+					reject(err);
+				}
+			};
+			fileReader.onerror = (e) => reject(e);
+			isLoading = true;
+			fileReader.readAsDataURL(file);
+		});
+	}
+
+	function parseOutput(body: unknown): string {
+		if (body && typeof body === "object" && body instanceof Blob) {
+			return URL.createObjectURL(body);
+		}
+		throw new TypeError(
+			"Invalid output: output must be of type object & of instance Blob"
+		);
+	}
+
+	function previewInputSample(sample: Record<string, any>) {
+		prompt = sample.prompt;
+		imgSrc = sample.src;
+	}
+
+	async function applyInputSample(sample: Record<string, any>) {
+		prompt = sample.prompt;
+		imgSrc = sample.src;
+		const res = await fetch(imgSrc);
+		const blob = await res.blob();
+		await updateImageBase64(blob);
+		getOutput();
+	}
+
+	async function getOutput({
+		withModelLoading = false,
+		isOnLoadCall = false,
+	} = {}) {
+		const trimmedPrompt = prompt.trim();
+
+		if (!imageBase64) {
+			error = "You need to upload an image";
+			output = "";
+			outputJson = "";
+			return;
+		}
+
+		const requestBody = {
+			image: imageBase64,
+			parameters: {
+				inputs: trimmedPrompt,
+			},
+		};
+		addInferenceParameters(requestBody, model);
+
+		isLoading = true;
+
+		const res = await getResponse(
+			apiUrl,
+			model.id,
+			requestBody,
+			apiToken,
+			parseOutput,
+			withModelLoading,
+			includeCredentials,
+			isOnLoadCall
+		);
+
+		isLoading = false;
+		// Reset values
+		computeTime = "";
+		error = "";
+		modelLoading = { isLoading: false, estimatedTime: 0 };
+		output = "";
+		outputJson = "";
+
+		if (res.status === "success") {
+			computeTime = res.computeTime;
+			output = res.output;
+			outputJson = res.outputJson;
+		} else if (res.status === "loading-model") {
+			modelLoading = {
+				isLoading: true,
+				estimatedTime: res.estimatedTime,
+			};
+			getOutput({ withModelLoading: true });
+		} else if (res.status === "error") {
+			error = res.error;
+		}
+	}
+
+	onMount(() => {
+		(async () => {
+			const [prompt_, src] = getDemoInputs(model, ["prompt", "src"]);
+			if (callApiOnMount && prompt_ && src) {
+				prompt = prompt_;
+				imgSrc = src;
+				const res = await fetch(imgSrc);
+				const blob = await res.blob();
+				await updateImageBase64(blob);
+				getOutput({ isOnLoadCall: true });
+			}
+		})();
+	});
+</script>
+
+<WidgetWrapper
+	{apiUrl}
+	{applyInputSample}
+	{computeTime}
+	{error}
+	{isLoading}
+	{model}
+	{modelLoading}
+	{noTitle}
+	{outputJson}
+	{previewInputSample}
+>
+	<svelte:fragment slot="top">
+		<form class="space-y-2">
+			<WidgetDropzone
+				classNames="no-hover:hidden"
+				{isLoading}
+				{imgSrc}
+				{onSelectFile}
+				onError={(e) => (error = e)}
+			>
+				{#if imgSrc}
+					<img
+						src={imgSrc}
+						class="pointer-events-none shadow mx-auto max-h-44"
+						alt=""
+					/>
+				{/if}
+			</WidgetDropzone>
+			<!-- Better UX for mobile/table through CSS breakpoints -->
+			{#if imgSrc}
+				{#if imgSrc}
+					<div
+						class="mb-2 flex justify-center bg-gray-50 dark:bg-gray-900 with-hover:hidden"
+					>
+						<img src={imgSrc} class="pointer-events-none max-h-44" alt="" />
+					</div>
+				{/if}
+			{/if}
+			<WidgetFileInput
+				accept="image/*"
+				classNames="mr-2 with-hover:hidden"
+				{isLoading}
+				label="Browse for image"
+				{onSelectFile}
+			/>
+			<WidgetTextInput
+				bind:value={prompt}
+				label="(Optional) Text-guidance if the model has support for it"
+				placeholder="Your prompt here..."
+			/>
+			<WidgetSubmitBtn
+				{isLoading}
+				onClick={() => {
+					getOutput();
+				}}
+			/>
+		</form>
+	</svelte:fragment>
+	<svelte:fragment slot="bottom">
+		{#if output.length}
+			<div class="flex justify-center mt-4 bg-gray-50 dark:bg-gray-925">
+				<img class="max-w-sm object-contain" src={output} alt="" />
+			</div>
+		{/if}
+	</svelte:fragment>
+</WidgetWrapper>
