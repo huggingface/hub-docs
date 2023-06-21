72b830ebcd6d6e776f1bc5652283cfb4f1578718
Merve Noyan <merveenoyan@gmail.com>
2023-06-21 12:15:00 +0300

    Add falcon-7b-instruct

--- tasks/src/text-generation/data.ts
+++ tasks/src/text-generation/data.ts
@@ -105,7 +105,7 @@ const taskData: TaskDataCustom = {
 		},
 	],
 	summary:      "Generating text is the task of producing new text. These models can, for example, fill in incomplete text or paraphrase.",
-	widgetModels: ["bigscience/bloom-560m"],
+	widgetModels: ["tiiuae/falcon-7b-instruct"],
 	youtubeId:    "Vpjb1lu0MDk",
 };
 
