280baa64a4035803ca84840632ca985d37537f0e
Julien Chaumond <julien@huggingface.co>
2023-06-23 10:44:59 +0100

    one less redirect

--- js/src/lib/components/InferenceWidget/shared/WidgetHeader/WidgetHeader.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetHeader/WidgetHeader.svelte
@@ -25,7 +25,7 @@
 				<IconLightning classNames="-ml-1 mr-1 text-yellow-500" />
 				Hosted inference API
 			</div>
-			<a target="_blank" href="https://api-inference.huggingface.co/">
+			<a target="_blank" href="https://huggingface.co/docs/api-inference">
 				<IconInfo classNames="ml-1.5 text-sm text-gray-400 hover:text-black" />
 			</a>
 		{/if}
