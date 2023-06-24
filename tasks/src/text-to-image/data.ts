03bb68f4a65a4da05c99f4225e1b2d18e78be8ef
Jayant <jayantyadav202@gmail.com>
2023-06-24 07:28:07 +0200

    Update data.ts
    removed newlines which were breaking texts

--- tasks/src/text-to-image/data.ts
+++ tasks/src/text-to-image/data.ts
@@ -28,18 +28,15 @@ const taskData: TaskDataCustom = {
 	},
 	metrics: [
 		{
-			description: "The Inception Score (IS) measures assesses diversity and meaningfulness. It uses a generated image sample to predict its label. 
-A higher score signifies more diverse and meaningful images.",
+			description: "The Inception Score (IS) measure assesses diversity and meaningfulness. It uses a generated image sample to predict its label. A higher score signifies more diverse and meaningful images.",
 			id: "IS",
 		},
 		{
-			description: "The Fréchet Inception Distance (FID) 
-calculates the distance between distributions between synthetic and real samples. A lower FID score indicates better similarity between the distributions of real and generated images.",
+			description: "The Fréchet Inception Distance (FID) calculates the distance between distributions between synthetic and real samples. A lower FID score indicates better similarity between the distributions of real and generated images.",
 			id: "FID",
 		},	
 		{
-			description: "R-precision assesses how the generated image aligns with the provided text description. It uses the generated images as queries to retrieve relevant text descriptions. 
-The top 'r' relevant descriptions are selected and used to calculate R-precision as r/R, where 'R' is the number of ground truth descriptions associated with the generated images. A higher R-precision value indicates a better model.",
+			description: "R-precision assesses how the generated image aligns with the provided text description. It uses the generated images as queries to retrieve relevant text descriptions. The top 'r' relevant descriptions are selected and used to calculate R-precision as r/R, where 'R' is the number of ground truth descriptions associated with the generated images. A higher R-precision value indicates a better model.",
 			id: "R-Precision",
 		},	
 		],
