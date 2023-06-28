aba09bf218d1c6d934aa26e8cbdf9ec9530ada90
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2023-06-28 19:58:50 +0530

    Shortening the description Notes

--- tasks/src/text-to-video/data.ts
+++ tasks/src/text-to-video/data.ts
@@ -3,31 +3,31 @@ import type { TaskDataCustom } from "../Types";
 const taskData: TaskDataCustom = {
 	datasets: [
         {
-			description: "(Microsoft Research Video to Text) is a large-scale dataset for the open domain video captioning, which consists of 10,000 video clips from 20 categories, and each video clip is annotated with 20 English sentences.There are about 29,000 unique words in all captions. The standard splits uses 6,513 clips for training, 497 clips for validation, and 2,990 clips for testing.",
+			description: "(Microsoft Research Video to Text) is a large-scale dataset for the open domain video captioning",
 			id:          "iejMac/CLIP-MSR-VTT",
 		},
         {
-			description: "(UCF101 Human Actions dataset) is an extension of UCF50 and consists of 13,320 video clips, which are classified into 101 categories. These 101 categories can be classified into 5 types (Body motion, Human-human interactions, Human-object interactions, Playing musical instruments and Sports). The total length of these video clips is over 27 hours. All the videos are collected from YouTube and have a fixed frame rate of 25 FPS with the resolution of 320 × 240.",
+			description: "(UCF101 Human Actions dataset) consists of 13,320 video clips from youtube, which are classified into 101 categories.",
 			id:          "quchenyuan/UCF101-ZIP",
 		},
         {
-			description: "Kinetics Human Action Video Dataset is a large-scale, high-quality dataset for human action recognition in videos. The dataset consists of around 500,000 video clips covering 600 human action classes with at least 600 video clips for each action class. Each video clip lasts around 10 seconds and is labeled with a single action class. The videos are collected from YouTube.",
+			description: "Kinetics Human Action Video Dataset is a large-scale, high-quality dataset for human action recognition in Youtube videos.",
 			id:          "nateraw/kinetics",
 		},
         {
-			description: "The 20BN-SOMETHING-SOMETHING V2 dataset is a large collection of labeled video clips that show humans performing pre-defined basic actions with everyday objects.  It contains 220,847 videos, with 168,913 in the training set, 24,777 in the validation set and 27,157 in the test set. There are 174 labels.",
+			description: "The 20BN-SOMETHING-SOMETHING V2 dataset of labeled(174) video clips that show humans performing pre-defined basic actions with everyday objects.",
 			id:          "HuggingFaceM4/something_something_v2",
 		},
         {
-			description: "This dataset consists of 10.7 million pairs of text-video pairs (52K video hours) and contains a fair amount of noisy samples with irrelevant video descriptions",
+			description: "This dataset consists of text-video pairs and contains a fair amount of noisy samples with irrelevant video descriptions",
 			id:          "HuggingFaceM4/webvid",
 		},
         {
-			description: "This dataset consists of 136M video clips with captions that describe complex tasks with step-by-step information on how to perform 23k activities from domains such as cooking, hand crafting, personal care, gardening or fitness. It has 136M video clips with captions sourced from 1.2M Youtube videos (15 years of video)",
+			description: "136M video clips with captions sourced from 1.2M Youtube videos (15 years of video) that describe complex tasks with step-by-step information",
 			id:          "totuta/youtube_subs_howto100M",
 		},
         {
-			description: "The Distinct Describable Moments (DiDeMo) dataset is one of the largest and most diverse datasets for the temporal localization of events in videos given natural language descriptions. The videos are collected from Flickr and each video is trimmed to a maximum of 30 seconds.",
+			description: "The Distinct Describable Moments (DiDeMo) has 30 sec Flickr videos for the temporal localization of events in videos given descriptions.",
 			id:          "iejMac/CLIP-DiDeMo",
 		},
     ],
