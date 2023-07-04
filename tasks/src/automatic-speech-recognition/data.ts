bb1ee995caa29b5db39030818ec1ad7ea335bdfb
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-07-04 15:00:22 +0200

    updating the model order + datasets

--- tasks/src/automatic-speech-recognition/data.ts
+++ tasks/src/automatic-speech-recognition/data.ts
@@ -3,15 +3,15 @@ import type { TaskDataCustom } from "../Types";
 const taskData: TaskDataCustom = {
 	datasets: [
 		{
+			description: "18,000 hours of multilingual audio-text dataset in 108 languages.",
+			id:          "mozilla-foundation/common_voice_13_0",
+		},
+		{
 			description: "An English dataset with 1,000 hours of data.",
 			id:          "librispeech_asr",
 		},
 		{
-			description: "Dataset in 60 languages including demographic information.",
-			id:          "common_voice",
-		},
-		{
-			description: "High quality, multi-speaker audio data and their transcriptions  in various languages.",
+			description: "High quality, multi-speaker audio data and their transcriptions in various languages.",
 			id:          "openslr",
 		},
 	],
@@ -43,15 +43,15 @@ const taskData: TaskDataCustom = {
 	],
 	models: [
 		{
-			description: "A good generic ASR model.",
-			id:          "facebook/wav2vec2-base-960h",
-		},
-		{
-			description: "A powerful ASR model.",
+			description: "A powerful ASR model by OpenAI.",
 			id:          "openai/whisper-large-v2",
 		},
 		{
-			description: "An end-to-end model that performs Automatic Speech Recognition and Speech Translation.",
+			description: "A good generic ASR model by MetaAI.",
+			id:          "facebook/wav2vec2-base-960h",
+		},
+		{
+			description: "An end-to-end model that performs ASR and Speech Translation.",
 			id:          "facebook/s2t-small-mustc-en-fr-st",
 		},
 	],
@@ -65,8 +65,8 @@ const taskData: TaskDataCustom = {
 			id:          "sanchit-gandhi/whisper-jax",
 		},
 		{
-			description: "An application that transcribes speeches in YouTube videos.",
-			id:          "jeffistyping/Youtube-Whisperer",
+			description: "A simple and controllable model for music generation.",
+			id:          "facebook/MusicGen",
 		},
 	],
 	summary:      "Automatic Speech Recognition (ASR), also known as Speech to Text (STT), is the task of transcribing a given audio to text. It has many applications, such as voice user interfaces.",
