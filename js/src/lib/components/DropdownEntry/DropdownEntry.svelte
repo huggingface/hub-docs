c9422bdf37a6aa181d2afa74aba05e723b0c1f87
Bertrand CHEVRIER <bertrand@huggingface.co>
2023-08-24 17:59:52 +0200

    chore: prettier...

--- js/src/lib/components/DropdownEntry/DropdownEntry.svelte
+++ js/src/lib/components/DropdownEntry/DropdownEntry.svelte
@@ -11,36 +11,43 @@
 	export let onClick: (e: MouseEvent) => void = () => {};
 	export let targetBlank = false;
 	export let type: "link" | "button" | "submit" | undefined = undefined;
-	
-	const element = type === undefined 
-		? href !== undefined ?  "a" : "button"
-		: type === "link" ? "a" : "button";
+
+	const element =
+		type === undefined
+			? href !== undefined
+				? "a"
+				: "button"
+			: type === "link"
+			? "a"
+			: "button";
 </script>
 
 <li>
-	<svelte:element 
-		this={element}
+	<svelte:element
 		class="flex items-center w-full hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer px-3 py-1.5 whitespace-nowrap
 		{classNames} {underline ? 'hover:underline' : ''}"
 		{href}
 		rel={element === "a" && noFollow ? "nofollow" : undefined}
 		target={element === "a" && targetBlank ? "_blank" : undefined}
 		on:click={(e) => {
-			if(type === "submit") {
+			if (type === "submit") {
 				e.stopPropagation();
 			}
 			onClick(e);
 		}}
 		type={element === "button" ? type : undefined}
 	>
-	<!-- Adding children to the DropdownEntry element overwrite the default label/icon stuff -->
-	{#if $$slots.default}
-		<slot />
-	{:else}
-		{#if icon}
-			<svelte:component this={icon} classNames="mr-1.5 flex-none {iconClassNames}" />
+		<!-- Adding children to the DropdownEntry element overwrite the default label/icon stuff -->
+		{#if $$slots.default}
+			<slot />
+		{:else}
+			{#if icon}
+				<svelte:component
+					this={icon}
+					classNames="mr-1.5 flex-none {iconClassNames}"
+				/>
+			{/if}
+			<span class="truncate">{label}</span>
 		{/if}
-		<span class="truncate">{label}</span>
-	{/if}
 	</svelte:element>
 </li>
