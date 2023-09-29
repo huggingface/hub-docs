a2f90b3fa33be51917e777379d27c24c5d35040c
Julien Chaumond <julien@huggingface.co>
2023-09-29 19:33:13 +0200

    more logical order..

--- js/src/lib/components/InferenceWidget/shared/WidgetExample.ts
+++ js/src/lib/components/InferenceWidget/shared/WidgetExample.ts
@@ -23,7 +23,6 @@ export type WidgetExampleOutput =
 export interface WidgetExampleBase<TOutput> {
 	example_title?: string;
 	group?:         string;
-	output?:        TOutput;
 	/**
 	 * Potential overrides to API parameters for this specific example
 	 * (takes precedences over the model card metadata's inference.parameters)
@@ -42,6 +41,10 @@ export interface WidgetExampleBase<TOutput> {
 		guidance_scale?:       number;
 		num_inference_steps?:  number;
 	};
+	/**
+	 * Optional output
+	 */
+	output?: TOutput;
 }
 
 export interface WidgetExampleTextInput<TOutput = WidgetExampleOutput> extends WidgetExampleBase<TOutput> {
