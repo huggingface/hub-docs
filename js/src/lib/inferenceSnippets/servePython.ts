7d63cbd5e891800623aac0b6f0834ca16015f308
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-10-16 15:30:13 +0200

    up

--- js/src/lib/inferenceSnippets/servePython.ts
+++ js/src/lib/inferenceSnippets/servePython.ts
@@ -43,7 +43,7 @@ from PIL import Image
 image = Image.open(io.BytesIO(image_bytes))`;
 
 export const snippetTextToAudio = (model: ModelData): string => {
-	if (ModelData.library_name === "transformers") {
+	if (model.library_name === "transformers") {
 		return `def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
