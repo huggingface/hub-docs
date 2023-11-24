dab07c45d044af66f969b5add74dd4559c7d53dc
Julien Chaumond <julien@huggingface.co>
2023-11-24 15:00:26 +0100

    rm everything

--- js/src/lib/components/InferenceWidget/stores.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-import type { ModelLoadInfo } from "./shared/types";
-
-export const modelLoadStates = writable<Record<string, ModelLoadInfo>>({});
--- js/src/routes/__layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import "../tailwind.css";
-</script>
-
-<div class="min-h-screen bg-gray-50 text-black dark:bg-gray-950">
-	<slot />
-</div>
