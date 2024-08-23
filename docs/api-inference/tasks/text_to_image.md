f6b31a80a8527a30fd3ba7ea9e92a361ece3fa59
Lucain <lucain@huggingface.co>
2024-08-23 15:29:42 +0200

    Generate tasks pages from script (#1386)
    * init project
    
    * first script to generate task pages
    
    * commit generated content
    
    * generate payload table as well
    
    * so undecisive
    
    * hey
    
    * better ?
    
    * Add image-to-image page
    
    * template for snippets section + few things
    
    * few things

---
+++ docs/api-inference/tasks/text_to_image.md
@@ -0,0 +1,116 @@
+## Text-to-image
+
+Generate an image based on a given text prompt.
+
+<Tip>
+
+For more details about the `text-to-image` task, check out its [dedicated page](https://huggingface.co/tasks/text-to-image)! You will find examples and related materials.
+
+</Tip>
+
+### Recommended models
+
+- [black-forest-labs/FLUX.1-dev](https://huggingface.co/black-forest-labs/FLUX.1-dev): One of the most powerful image generation models that can generate realistic outputs.
+- [latent-consistency/lcm-lora-sdxl](https://huggingface.co/latent-consistency/lcm-lora-sdxl): A powerful yet fast image generation model.
+- [Kwai-Kolors/Kolors](https://huggingface.co/Kwai-Kolors/Kolors): Text-to-image model for photorealistic generation.
+- [stabilityai/stable-diffusion-3-medium-diffusers](https://huggingface.co/stabilityai/stable-diffusion-3-medium-diffusers): A powerful text-to-image model.
+
+This is only a subset of the supported models. Find the model that suits you best [here](https://huggingface.co/models?inference=warm&pipeline_tag=text-to-image&sort=trending).
+
+### API specification
+
+#### Request
+
+| Payload |  |  |
+| :--- | :--- | :--- |
+| **inputs** | _string, required_ | The input text data (sometimes called "prompt" |
+| **parameters** | _object, optional_ | Additional inference parameters for Text To Image |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;guidance_scale** | _number, optional_ | For diffusion models. A higher guidance scale value encourages the model to generate images closely linked to the text prompt at the expense of lower image quality. |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;negative_prompt** | _array, optional_ | One or several prompt to guide what NOT to include in image generation. |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;num_inference_steps** | _integer, optional_ | For diffusion models. The number of denoising steps. More denoising steps usually lead to a higher quality image at the expense of slower inference. |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;target_size** | _object, optional_ | The size in pixel of the output image |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;width** | _integer, required_ |  |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;height** | _integer, required_ |  |
+| **&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;scheduler** | _string, optional_ | For diffusion models. Override the scheduler with a compatible one |
+
+
+| Headers |   |    |
+| :--- | :--- | :--- |
+| **authorization** | _string, optional_ | Authentication header in the form `'Bearer: hf_****'` when `hf_****` is a personal user access token with Inference API permission. You can generate one from [your settings page](https://huggingface.co/settings/tokens). |
+| **x-use-cache** | _boolean, optional, default to `true`_ | There is a cache layer on the inference API to speed up requests we have already seen. Most models can use those results as they are deterministic (meaning the outputs will be the same anyway). However, if you use a nondeterministic model, you can set this parameter to prevent the caching mechanism from being used, resulting in a real new query. Read more about caching [here](../parameters#additional-parameters-different-page]). |
+| **x-wait-for-model** | _boolean, optional, default to `false`_ | If the model is not ready, wait for it instead of receiving 503. It limits the number of requests required to get your inference done. It is advised to only set this flag to true after receiving a 503 error, as it will limit hanging in your application to known places. Read more about model availability [here](../parameters#additional-parameters-different-page]). |
+
+
+#### Response
+
+| Body |  |
+| :--- | :--- |
+| **image** | The generated image |
+
+
+### Using the API
+
+
+<inferencesnippet>
+
+<curl>
+```bash
+curl https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev \
+	-X POST \
+	-d '{"inputs": "Astronaut riding a horse"}' \
+	-H 'Content-Type: application/json' \
+	-H "Authorization: Bearer hf_***"
+
+```
+</curl>
+
+<python>
+```py
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+headers = {"Authorization": "Bearer hf_***"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": "Astronaut riding a horse",
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+```
+
+To use the Python client, see `huggingface_hub`'s [package reference](https://huggingface.co/docs/huggingface_hub/package_reference/inference_client#huggingface_hub.InferenceClient.text_to-image).
+</python>
+
+<js>
+```js
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
+		{
+			headers: {
+				Authorization: "Bearer hf_***"
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+query({"inputs": "Astronaut riding a horse"}).then((response) => {
+	// Use image
+});
+```
+
+To use the JavaScript client, see `huggingface.js`'s [package reference](https://huggingface.co/docs/huggingface.js/inference/classes/HfInference#textto-image).
+</js>
+
+</inferencesnippet>
+
+
---
+++ scripts/api-inference/scripts/generate.ts
@@ -0,0 +1,301 @@
+import { snippets, PipelineType } from "@huggingface/tasks";
+import Handlebars from "handlebars";
+import * as fs from "node:fs/promises";
+import * as path from "node:path/posix";
+import type { JsonObject } from "type-fest";
+
+const inferenceSnippetLanguages = ["python", "js", "curl"] as const;
+type InferenceSnippetLanguage = (typeof inferenceSnippetLanguages)[number];
+
+// Taken from https://stackoverflow.com/a/31632215
+Handlebars.registerHelper({
+  eq: (v1, v2) => v1 === v2,
+  ne: (v1, v2) => v1 !== v2,
+  lt: (v1, v2) => v1 < v2,
+  gt: (v1, v2) => v1 > v2,
+  lte: (v1, v2) => v1 <= v2,
+  gte: (v1, v2) => v1 >= v2,
+  and() {
+    return Array.prototype.every.call(arguments, Boolean);
+  },
+  or() {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+  },
+});
+
+console.log("🛠️  Preparing...");
+
+////////////////////////
+//// Filepath utils ////
+////////////////////////
+
+const ROOT_DIR = path
+  .join(path.normalize(import.meta.url), "..", "..")
+  .replace(/^(file:)/, "");
+const TEMPLATE_DIR = path.join(ROOT_DIR, "templates");
+const DOCS_DIR = path.join(ROOT_DIR, "..", "..", "docs");
+const TASKS_DOCS_DIR = path.join(DOCS_DIR, "api-inference", "tasks");
+
+function readTemplate(templateName: string): Promise<string> {
+  const templateNameSnakeCase = templateName.replace(/-/g, "_");
+  const templatePath = path.join(
+    TEMPLATE_DIR,
+    `${templateNameSnakeCase}.handlebars`,
+  );
+  console.debug(`   🔍 Reading ${templateNameSnakeCase}.handlebars`);
+  return fs.readFile(templatePath, { encoding: "utf-8" });
+}
+
+function writeTaskDoc(templateName: string, content: string): Promise<void> {
+  const templateNameSnakeCase = templateName.replace(/-/g, "_");
+  const taskDocPath = path.join(TASKS_DOCS_DIR, `${templateNameSnakeCase}.md`);
+  console.debug(`   💾 Saving to ${taskDocPath}`);
+  return fs
+    .mkdir(TASKS_DOCS_DIR, { recursive: true })
+    .then(() => fs.writeFile(taskDocPath, content, { encoding: "utf-8" }));
+}
+
+/////////////////////////
+//// Task page utils ////
+/////////////////////////
+
+const TASKS_API_URL = "https://huggingface.co/api/tasks";
+console.debug(`   🕸️ Fetching ${TASKS_API_URL}`);
+const response = await fetch(TASKS_API_URL);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const TASKS_DATA = (await response.json()) as any;
+
+///////////////////////
+//// Snippet utils ////
+///////////////////////
+
+const GET_SNIPPET_FN = {
+  curl: snippets.curl.getCurlInferenceSnippet,
+  js: snippets.js.getJsInferenceSnippet,
+  python: snippets.python.getPythonInferenceSnippet,
+} as const;
+
+const HAS_SNIPPET_FN = {
+  curl: snippets.curl.hasCurlInferenceSnippet,
+  js: snippets.js.hasJsInferenceSnippet,
+  python: snippets.python.hasPythonInferenceSnippet,
+} as const;
+
+export function getInferenceSnippet(
+  id: string,
+  pipeline_tag: PipelineType,
+  language: InferenceSnippetLanguage,
+): string | undefined {
+  const modelData = {
+    id,
+    pipeline_tag,
+    mask_token: "",
+    library_name: "",
+    config: {},
+  };
+  if (HAS_SNIPPET_FN[language](modelData)) {
+    return GET_SNIPPET_FN[language](modelData, "hf_***");
+  }
+}
+
+/////////////////////
+//// Specs utils ////
+/////////////////////
+
+type SpecNameType = "input" | "output" | "stream_output";
+
+const SPECS_URL_TEMPLATE = Handlebars.compile(
+  `https://raw.githubusercontent.com/huggingface/huggingface.js/main/packages/tasks/src/tasks/{{task}}/spec/{{name}}.json`,
+);
+
+async function fetchOneSpec(
+  task: PipelineType,
+  name: SpecNameType,
+): Promise<JsonObject | undefined> {
+  const url = SPECS_URL_TEMPLATE({ task, name });
+  console.debug(`   🕸️ Fetching ${task} ${name} specs`);
+  return fetch(url)
+    .then((res) => res.json())
+    .catch(() => undefined);
+}
+
+async function fetchSpecs(
+  task: PipelineType,
+): Promise<
+  Record<"input" | "output" | "stream_output", JsonObject | undefined>
+> {
+  return {
+    input: await fetchOneSpec(task, "input"),
+    output: await fetchOneSpec(task, "output"),
+    stream_output: await fetchOneSpec(task, "stream_output"),
+  };
+}
+
+function processPayloadSchema(schema: any, prefix: string = ""): JsonObject[] {
+  let rows: JsonObject[] = [];
+
+  Object.entries(schema.properties || {}).forEach(
+    ([key, value]: [string, any]) => {
+      const isRequired = schema.required?.includes(key);
+      let type = value.type || "object";
+
+      if (value.$ref) {
+        // Handle references
+        const refSchemaKey = value.$ref.split("/").pop();
+        value = schema.$defs?.[refSchemaKey!];
+      }
+
+      const description = value.description || "";
+      const isObject = type === "object" && value.properties;
+      const row = {
+        name: `${prefix}${key}`,
+        type: type,
+        description: description,
+        required: isRequired ? "required" : "optional",
+      };
+      rows.push(row);
+
+      if (isObject) {
+        // Recursively process nested objects
+        rows = rows.concat(
+          processPayloadSchema(
+            value,
+            prefix + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",
+          ),
+        );
+      }
+    },
+  );
+
+  return rows;
+}
+
+//////////////////////////
+//// Inline templates ////
+//////////////////////////
+
+const TIP_LINK_TO_TASK_PAGE_TEMPLATE = Handlebars.compile(`<Tip>
+
+For more details about the \`{{task}}\` task, check out its [dedicated page](https://huggingface.co/tasks/{{task}})! You will find examples and related materials.
+
+</Tip>`);
+
+const TIP_LIST_MODELS_LINK_TEMPLATE = Handlebars.compile(
+  `This is only a subset of the supported models. Find the model that suits you best [here](https://huggingface.co/models?inference=warm&pipeline_tag={{task}}&sort=trending).`,
+);
+
+const SPECS_HEADERS = await readTemplate("specs-headers");
+const SNIPPETS_TEMPLATE = Handlebars.compile(
+  await readTemplate("snippets-template"),
+);
+const SPECS_PAYLOAD_TEMPLATE = Handlebars.compile(
+  await readTemplate("specs-payload"),
+);
+const SPECS_OUTPUT_TEMPLATE = Handlebars.compile(
+  await readTemplate("specs-output"),
+);
+
+////////////////////
+//// Data utils ////
+////////////////////
+
+const TASKS: PipelineType[] = ["image-to-image", "text-to-image"];
+
+const DATA: {
+  constants: {
+    specsHeaders: string;
+  };
+  models: Record<string, { id: string; description: string }[]>;
+  snippets: Record<string, string>;
+  specs: Record<
+    string,
+    {
+      input: string | undefined;
+      output: string | undefined;
+      stream_output: string | undefined;
+    }
+  >;
+  tips: {
+    linksToTaskPage: Record<string, string>;
+    listModelsLink: Record<string, string>;
+  };
+} = {
+  constants: {
+    specsHeaders: SPECS_HEADERS,
+  },
+  models: {},
+  snippets: {},
+  specs: {},
+  tips: { linksToTaskPage: {}, listModelsLink: {} },
+};
+
+// Fetch recommended models
+TASKS.forEach((task) => {
+  // TODO loop over models and filter out freezed ones
+  DATA.models[task] = TASKS_DATA[task].models;
+});
+
+// Fetch snippets
+// TODO: render snippets only if they are available
+TASKS.forEach((task) => {
+  const mainModel = TASKS_DATA[task].models[0].id;
+  const taskSnippets = {
+    curl: getInferenceSnippet(mainModel, task, "curl"),
+    python: getInferenceSnippet(mainModel, task, "python"),
+    javascript: getInferenceSnippet(mainModel, task, "js"),
+  };
+  DATA.snippets[task] = SNIPPETS_TEMPLATE({
+    taskSnippets,
+    taskSnakeCase: task.replace("-", "_"),
+    taskAttached: task.replace("-", ""),
+  });
+});
+
+// Render specs
+await Promise.all(
+  TASKS.map(async (task) => {
+    const specs = await fetchSpecs(task);
+    DATA.specs[task] = {
+      input: specs.input
+        ? SPECS_PAYLOAD_TEMPLATE({ schema: processPayloadSchema(specs.input) })
+        : undefined,
+      output: specs.output
+        ? SPECS_OUTPUT_TEMPLATE({ schema: processPayloadSchema(specs.output) })
+        : undefined,
+      stream_output: specs.stream_output
+        ? SPECS_OUTPUT_TEMPLATE({
+            schema: processPayloadSchema(specs.stream_output),
+          })
+        : undefined,
+    };
+  }),
+);
+
+// Render tips
+TASKS.forEach((task) => {
+  DATA.tips.linksToTaskPage[task] = TIP_LINK_TO_TASK_PAGE_TEMPLATE({ task });
+  DATA.tips.listModelsLink[task] = TIP_LIST_MODELS_LINK_TEMPLATE({ task });
+});
+
+/////////////////////////
+//// Rendering utils ////
+/////////////////////////
+
+async function renderTemplate(
+  templateName: string,
+  data: JsonObject,
+): Promise<string> {
+  console.log(`🎨  Rendering ${templateName}`);
+  const template = Handlebars.compile(await readTemplate(templateName));
+  return template(data);
+}
+
+await Promise.all(
+  TASKS.map(async (task) => {
+    // @ts-ignore
+    const rendered = await renderTemplate(task, DATA);
+    await writeTaskDoc(task, rendered);
+  }),
+);
+
+console.log("✅ All done!");
